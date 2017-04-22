fb7a5032da295dbd9942d4377bd80e0c8a79bf63
Philipp Dowling <phdowling@users.noreply.github.com>
2017-04-22 12:44:54 -0700

    setting up nginx

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 __author__ = 'dowling'
 from inquire_web.server import app
+from inquire_web.blueprints.query import init_query_blueprint
 from werkzeug.wsgi import DispatcherMiddleware
 
+with open("backend_servers.txt", "r") as inf:
+    servers = [line.strip() for line in inf.readlines() if line.strip()]
+    init_query_blueprint(servers)
 
 def placeholder(env, resp):
     resp(b'200 OK', [(b'Content-Type', b'text/plain')])
